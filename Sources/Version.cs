1e14219b5892510c97cddd0f2c9e01d5d7c0d709
Peter Laudy <peter@dot42.com>
2015-04-15 22:44:55 +0200

    Updated version and changelog to reflect changes made last January.

--- Sources/Version.cs
+++ Sources/Version.cs
@@ -2,5 +2,5 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("1.0.1.81")]
+[assembly: AssemblyVersion("1.0.1.82")]
 [assembly: AssemblyConfiguration("")]
