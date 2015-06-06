72a7aec5507860a44678bbdbfb18b20e8008970f
Olaf Pettersson <olaf.pettersson@web.de>
2015-06-06 13:32:41 +0200

    disabled broken loading of Runtime(In)VisibleParameterAnnotations from .class files

--- Sources/JvmClassLib/Structures/ConstantPoolTags.cs
+++ Sources/JvmClassLib/Structures/ConstantPoolTags.cs
@@ -2,16 +2,18 @@
 {
     public enum ConstantPoolTags
     {
-        Class = 7,
-        Fieldref =9,
-        Methodref = 10,
-        InterfaceMethodref = 11,
-        String = 8,
+        Utf8 = 1,
         Integer = 3,
         Float = 4,
         Long = 5,
         Double = 6,
+        Class = 7,
+        String = 8,
+        Fieldref = 9,
+        Methodref = 10,
+        InterfaceMethodref = 11,
         NameAndType = 12,
-        Utf8 = 1
+
+        
     }
 }
