08b4e740de394114490479df6cfd23cc88784e3f
Olaf Pettersson <olaf.pettersson@web.de>
2016-02-24 17:42:59 +0100

    bump version no

--- Sources/Version.cs
+++ Sources/Version.cs
@@ -2,5 +2,5 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("1.0.1.83")]
+[assembly: AssemblyVersion("2.0.1.1")]
 [assembly: AssemblyConfiguration("")]
